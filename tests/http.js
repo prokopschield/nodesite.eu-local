2d209e9dd99e1fc9d0269f15c24d0a67078bceee
Prokop <prokop.schield@protonmail.com>
2021-06-13 20:59:10 +0200

    Release 1.2.0

---
+++ tests/http.js
@@ -0,0 +1,14 @@
+const { listen } = require('..');
+
+module.exports = () => new Promise(resolve => {
+	listen({
+		name: 'test',
+		port: 8080,
+	}).create('/', (req) => (
+		console.log(`Received request at ${req.uri}`),
+		resolve(),
+		`Hello from nodesite.eu-local`
+	));
+	console.log(`Go to http://localhost:8080/`);
+	console.log(`Ignore the messages below.`);
+});
